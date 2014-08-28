3adc0e1b93c9c6bf778c2aa36f7578a1412fdd51
Jonathan Kemp <jonathan.kemp@cellularsales.com>
2014-08-28 09:44:45 -0400

    Fix connect task after upgrade to 3.0. Install missing modules. Replace static and directory methods with their modules.

--- gulpfile.js
+++ gulpfile.js
@@ -83,10 +83,12 @@ gulp.task('wiredep', function () {
 
 gulp.task('connect', function () {
     var connect = require('connect');
+    var serveStatic = require('serve-static');
+    var serveIndex = require('serve-index');
     var app = connect()
         .use(require('connect-livereload')({ port: 35729 }))
-        .use(connect.static('app'))
-        .use(connect.directory('app'));
+        .use(serveStatic('app'))
+        .use(serveIndex('app'));
 
     require('http').createServer(app)
         .listen(9000)
